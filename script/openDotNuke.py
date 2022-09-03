38bd5e235b433a831dd21cc127bd997a2de91a2c
Lundy Hu <33220975+isLundy@users.noreply.github.com>
2022-09-03 16:04:51 +0800

    Update openDotNuke.py

--- script/openDotNuke.py
+++ script/openDotNuke.py
@@ -1,9 +1,11 @@
+import nuke
+import sys
 import os
 import platform
 import subprocess
 
-def openDotNuke():
-    path = os.getenv('HOME') + "/.nuke"
+# open main
+def openExplorer(path):
     operatingSystem = platform.system()
 
     if operatingSystem == "Windows":
@@ -14,3 +16,28 @@ def openDotNuke():
 
     else:
         subprocess.Popen(["xdg-open", path])
+
+# open nk or file directory
+def openFileDir():
+
+    if len(nuke.selectedNodes()) != 0:
+        for i in nuke.selectedNodes():
+            try:
+                path = os.path.dirname(i.knob('file').evaluate())
+                openExplorer(path)
+            except:
+                pass
+
+    else:
+        path = os.path.dirname(nuke.root().name())
+        openExplorer(path)
+
+# open .nuke directory
+def openDotNuke():
+    path = os.getenv('HOME') + "/.nuke"
+    openExplorer(path)
+
+# open  nuke installation directory
+def openExePath():
+    path = os.path.dirname(nuke.EXE_PATH)
+    openExplorer(path)
